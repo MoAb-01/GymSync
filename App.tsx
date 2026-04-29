be542c42f2f5d9d926426274fb22bc5b795a11f3
MoAb-01 <mohamedgamalabdou0@gmail.com>
2026-04-29 17:20:33 +0300

    feat: integrate NativeWind and Tailwind CSS for styling into the project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-[#0F172A]">
+      <StatusBar style="light" />
+      <Text className="text-white text-2xl font-bold">GymSync</Text>
+      <Text className="text-slate-400 mt-2">
+        Dark Mode Base Setup Complete.
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
